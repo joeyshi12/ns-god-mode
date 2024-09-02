778211ac123dbaba4001a3f0078ab0ea19ad680f
Joey Shi <joeyshi12@hotmail.com>
2024-09-01 22:20:13 -0700

    draft

---
+++ GodMode/Patches/PlayerPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace GodMode.Patches
+{
+    public class PlayerPatch
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(Player), "Awake")]
+        public static void AwakePostFix(ref Player __instance)
+        {
+            __instance.health.OnTakeDamageDataEvent.RemoveAllListeners();
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(Player), "Update")]
+        public static void UpdatePostFix(ref Player __instance)
+        {
+            __instance.RestoreEverything();
+        }
+    }
+}
---
+++ GodMode/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GodMode.Patches;
+using HarmonyLib;
+
+namespace GodMode
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        const string modGUID = "coolcat0.NineSolsGodMode";
+        const string modName = "NineSolsGodMode";
+        const string modVersion = "0.1.0";
+
+        public static Plugin instance;
+        public static ManualLogSource mls;
+
+        void Awake()
+        {
+            instance = this;
+            Harmony.CreateAndPatchAll(typeof(PlayerPatch));
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo($"{modName} version {modVersion} has been loaded");
+        }
+    }
+}
